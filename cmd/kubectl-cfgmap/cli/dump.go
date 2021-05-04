f8b476c12842a8314e204cb6af440103e55531b5
Nick Young <ynick@vmware.com>
2021-05-04 10:29:40 +0000

    Make dump command work
    Signed-off-by: Nick Young <ynick@vmware.com>

--- cmd/kubectl-cfgmap/cli/dump.go
+++ cmd/kubectl-cfgmap/cli/dump.go
@@ -18,24 +18,74 @@ package cli
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/kubernetes"
 	cmdutil "k8s.io/kubectl/pkg/cmd/util"
 )
 
-func newDumpCmd(ctx context.Context, ioStreams genericclioptions.IOStreams, factory cmdutil.Factory) *cobra.Command {
+func newDumpCmd(ctx context.Context, ioStreams genericclioptions.IOStreams, f cmdutil.Factory) *cobra.Command {
 
 	// dumpCmd represents the dump command
 	var dumpCmd = &cobra.Command{
 		Use:   "dump",
 		Short: "Dump the contents of a ConfigMap to a directory as separate files.",
 		Long: `This application is a tool to dump the contents of a ConfigMap
-		to a directory as separate files, with the keys as the filenames.`,
+		to a directory as separate files.
+		
+		The directory will be created as <basedir>/<namespace>/<name>, with the
+		keys as the filenames.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("dump called")
+			config, err := f.ToRESTConfig()
+			cobra.CheckErr(err)
+
+			clientset, err := kubernetes.NewForConfig(config)
+			cobra.CheckErr(err)
+
+			namespace, _, err := f.ToRawKubeConfigLoader().Namespace()
+			cobra.CheckErr(err)
+			name := args[0]
+			configmap, err := clientset.CoreV1().ConfigMaps(namespace).Get(ctx, name, metav1.GetOptions{})
+			cobra.CheckErr(err)
+			// fmt.Fprint(ioStreams.Out, configmap)
+
+			basedir, err := cmd.Flags().GetString("basedir")
+			cobra.CheckErr(err)
+
+			fmt.Fprintf(ioStreams.Out, "Using directory %s/%s/%s\n", basedir, namespace, name)
+			errMkdir := os.MkdirAll(fmt.Sprintf("%s/%s/%s", basedir, namespace, name), 0777)
+			cobra.CheckErr(errMkdir)
+
+			for key, value := range configmap.Data {
+				fmt.Fprintf(ioStreams.Out, "Creating %s...", key)
+
+				filename := filepath.Join(basedir, namespace, name, key)
+
+				err := ioutil.WriteFile(filename, []byte(value), 0644)
+				cobra.CheckErr(err)
+				fmt.Fprint(ioStreams.Out, "Done\n")
+			}
+			// for _, namespace := range namespaces.Items {
+			// 	configmaps, err := clientset.CoreV1().ConfigMaps(namespace.Name).List(ctx, metav1.ListOptions{})
+			// 	if err != nil {
+			// 		cobra.CheckErr(err)
+			// 	}
+
+			// 	for _, configmap := range configmaps.Items {
+
+			// 	}
+
+			// }
+
+			// clientset, err :=
 		},
 	}
 
+	dumpCmd.Flags().String("basedir", ".", "Set the base directory for the configmap directory to be created in.")
 	return dumpCmd
 }
--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -6,21 +6,10 @@ import (
 
 	"github.com/pkg/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/kubernetes"
 )
 
-func RunPlugin(ctx context.Context, configFlags *genericclioptions.ConfigFlags, outputCh chan string) error {
-	config, err := configFlags.ToRESTConfig()
-	if err != nil {
-		return errors.Wrap(err, "failed to read kubeconfig")
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return errors.Wrap(err, "failed to create clientset")
-	}
-
+func RunPlugin(ctx context.Context, clientset kubernetes.Clientset, outputCh chan string) error {
 	namespaces, err := clientset.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
 	if err != nil {
 		return errors.Wrap(err, "failed to list namespaces")
